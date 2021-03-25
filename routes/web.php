6db2295a8bed630c466c710919a7498e63634d4e
Daniel2711 <detaddeo.d@gmail.com>
2021-03-25 19:25:29 +0100

    Sistemazione laravel auth

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeControllor@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');;
+
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+});
