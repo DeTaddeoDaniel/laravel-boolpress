44c4a1279ef2c9504db7fed1380e1eea28850bee
Daniel2711 <detaddeo.d@gmail.com>
2021-03-26 15:43:45 +0100

    Visualizza poster pagina - index guest post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,13 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
+
         $posts = Post::all();
-        dd($posts);
-        return view( 'guest.post.index' );
+        
+        $data = [
+            'posts' => $posts
+        ];
+        
+        return view( 'guest.post.index', $data );
     }
 }
