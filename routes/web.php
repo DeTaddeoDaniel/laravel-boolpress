fe9441c4a27d9ddf3abbb5a93e1e802be9274d36
Daniel2711 <detaddeo.d@gmail.com>
2021-03-26 15:12:29 +0100

    Creata rotta guest index posts

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('index');
+Route::get('/posts', 'PostController@index')->name('guest.post.index');
 
 Auth::routes();
 
