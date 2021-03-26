7c3de425ff0bed917697a63d78dbb38128d6e75d
Daniel2711 <detaddeo.d@gmail.com>
2021-03-26 16:23:48 +0100

    Modifiche grafiche - show guest post

--- resources/views/guest/post/index.blade.php
+++ resources/views/guest/post/index.blade.php
@@ -7,19 +7,23 @@
 
         <h1>Lista dei post</h1>
 
-        <div class="cards">
+        <div class="cards d-flex align-content-around flex-wrap">
             
             @foreach ($posts as $post)
-                
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$post->title}}</h5>
-                        <p class="card-text">{{$post->content}}</p>
-                        <h6 class="card-subtitle mb-2 text-muted">Autore: {{$post->user->name}}</h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Data: {{$post->updated_at}}</h6>
-                        <a href="{{route('guest.post.show', $post->slung)}}" class="card-link">Show</a>
-                        {{-- <a href="#" class="card-link">Another link</a> --}}
+
+                <div class="col-4 p-1">
+                    
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$post->title}}</h5>
+                            <p class="card-text">{{$post->content}}</p>
+                            <h6 class="card-subtitle mb-2 text-muted">Autore: {{$post->user->name}}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted">Data: {{$post->updated_at}}</h6>
+                            <a href="{{route('guest.post.show', $post->slung)}}" class="card-link">Show</a>
+                            {{-- <a href="#" class="card-link">Another link</a> --}}
+                        </div>
                     </div>
+                
                 </div>
 
             @endforeach
--- resources/views/guest/post/show.blade.php
+++ resources/views/guest/post/show.blade.php
@@ -7,7 +7,7 @@
 
         <h1>Show elemento blog</h1>   
                 
-                <div class="card w-50">
+                <div class="card w-100">
                     <div class="card-body">
                         <h5 class="card-title">{{$post->title}}</h5>
                         <p class="card-text">{{$post->content}}</p>
