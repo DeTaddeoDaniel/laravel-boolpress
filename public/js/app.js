ff353381e4142bd2b427ecdca3b60b0c977a97a0
Daniel2711 <detaddeo.d@gmail.com>
2021-03-26 22:19:00 +0100

    installazione admin lte

--- public/js/app.js
+++ public/js/app.js
@@ -49846,7 +49846,8 @@ module.exports = function(module) {
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js"); // require('admin-lte');
+
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 /**
